60b14f5a85543a95824587d6733383c33704b23a
Sammynator <sgerules@gmail.com>
2025-02-25 14:14:02 +0100

    created round mockups and finalized mockup generation

--- PROSCRUM-BACKEND/app/mockups.py
+++ PROSCRUM-BACKEND/app/mockups.py
@@ -1,4 +1,5 @@
-from app.models import HoleConfig, CourseWithID
+from app.models import HoleConfig, CourseWithID, RoundIn, RoundOut
+from app.calculations import start_calculations
 
 def generate_courses():
     courses_list = []
@@ -106,9 +107,139 @@ def generate_courses():
     courses_list.append(course_4)
     
     return courses_list
+    
 
+def generate_round_in(courses_list):
+    round_in = []
+    
+    course_1 = courses_list[0]
+
+    # Runde 1
+    round_1 = RoundIn(
+    course=course_1,
+    round_number=1,
+    scores=[5, 6, 8, 7, 6, 6, 6, 6, 6, 7, 6, 6, 5, 6, 6, 6, 5, 6]
+    )
+    round_in.append(round_1)
+
+    # Runde 2
+    round_2 = RoundIn(
+    course=course_1,
+    round_number=2,
+    scores=[4, 5, 5, 6, 6, 5, 6, 9, 5, 5, 6, 6, 5, 6, 6, 6, 5, 6]
+    )
+    round_in.append(round_2)
+
+    course_3 = courses_list[1]
+
+    round_3 = RoundIn(
+    course=course_3,
+    round_number=3,
+    scores=[4, 5, 5, 6, 6, 7, 4, 8, 4, 5, 6, 6, 5, 6, 6, 6, 5, 6]
+    )
+    round_in.append(round_3)
+
+    # Runde 4 (Verwendet das gleiche Course-Objekt wie für Runde 3)
+    round_4 = RoundIn(
+    course=course_3,
+    round_number=4,
+    scores=[5, 6, 6, 7, 6, 6, 4, 7, 6, 7, 6, 6, 5, 5, 6, 6, 5, 6]
+    )
+    round_in.append(round_4)
+
+    # Runde 5
+    round_5 = RoundIn(
+    course=course_1,
+    round_number=5,
+    scores=[4, 5, 5, 6, 5, 5, 4, 5, 5, 6, 5, 5, 4, 5, 5, 6, 5, 5]
+    )
+    round_in.append(round_5)
+
+    # Runde 6 (9 Löcher von Course 1)
+    course_6 = courses_list[2]
+
+    round_6 = RoundIn(
+    course=course_6,
+    round_number=6,
+    scores=[5, 6, 6, 7, 6, 5, 5, 6, 6]
+    )
+    round_in.append(round_6)
+
+    # Runde 7 (Verwendet das gleiche Course-Objekt wie für Runde 6)
+    round_7 = RoundIn(
+    course=course_6,
+    round_number=7,
+    scores=[5, 7, 9, 7, 6, 6, 6, 6, 6]
+    )
+    round_in.append(round_7)
+
+    # Runde 8 (Verwendet das gleiche Course-Objekt wie für Runde 1 und 5)
+    round_8 = RoundIn(
+    course=course_1,
+    round_number=8,
+    scores=[4, 5, 5, 6, 6, 5, 4, 5, 5, 7, 5, 5, 4, 5, 5, 7, 5, 5]
+    )
+    round_in.append(round_8)
+
+    # Runde 9 (Verwendet das gleiche Course-Objekt wie für Runde 3 und 4) glaube falsch
+    round_9 = RoundIn(
+    course=course_3,
+    round_number=9,
+    scores=[4, 5, 5, 6, 5, 5, 4, 5, 5, 6, 5, 5, 4, 5, 5, 6, 5, 5]
+    )
+    round_in.append(round_9)
+
+    # Runde 10 irgendwie course 1
+    course_10 = courses_list[3]
+    
+    round_10 = RoundIn(
+    course=course_10,
+    round_number=10,
+    scores=[4, 5, 5, 6, 5, 5, 4, 5, 5],
+    )
+    round_in.append(round_10)
+
+    # Runde 11 (Verwendet das gleiche Course-Objekt wie für Runde 10)
+    round_11 = RoundIn(
+    course=course_10,
+    round_number=11,
+    scores=[5, 6, 6, 7, 6, 6, 5, 6, 6],
+    )
+    round_in.append(round_11)
+
+    # Runde 12 (Verwendet das gleiche Course-Objekt wie für Runde 3, 4, 9 und 10)
+    round_12 = RoundIn(
+    course=course_3,
+    round_number=12,
+    scores=[6, 7, 7, 8, 7, 7, 6, 7, 10, 8, 7, 7, 6, 7, 7, 8, 7, 7],
+    )
+    round_in.append(round_12)
+    
+    return round_in
+
+def calculate_round_outs(round_ins: list[RoundIn]):
+    round_out_list = []
+    
+    for round in round_ins:
+        calc_result = start_calculations(round, round_out_list)
+        new_calc_result_2020 = calc_result[0]
+        new_calc_result_2021 = calc_result[1]
+        round_score_differential = calc_result[2]
+
+        new_round = RoundOut(
+            # is_old=round.is_old,
+            round_number=round.round_number, 
+            course=round.course, scores=round.scores, 
+            calc_result_2020=new_calc_result_2020, 
+            calc_result_2021=new_calc_result_2021, 
+            score_differential=round_score_differential
+        )
+        round_out_list.append(new_round)
+        
+    return round_out_list
 
 def generate_mockups():
     courses_list = generate_courses()
-
-    return courses_list
+    round_in_list = generate_round_in(courses_list)
+    round_out_list = calculate_round_outs(round_in_list)
+    return round_out_list
