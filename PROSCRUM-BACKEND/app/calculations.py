e537360de27b95b465d93fe3aee69256e4837b2b
notsambutrobin <robin.messer@stud-provadis-hochschule.de>
2025-03-05 12:14:04 +0100

    adapt function headers and allow better changability

--- PROSCRUM-BACKEND/app/calculations.py
+++ PROSCRUM-BACKEND/app/calculations.py
@@ -22,10 +22,14 @@ def start_calculations(new_round: RoundIn, old_rounds: list[RoundOut]):
     else:
         course_rating = current_course.course_rating_18
         nine_hole = False
+    slope_rating = current_course.slope_rating
+    course_par = current_course.course_par
+    scores = new_round.scores
+    holes_played = current_course.holes
 
     # calculate the handicaps
-    handicaps = [calculate_ega_handicap(old_hdc_2020, current_course.holes, current_course.slope_rating, course_rating, current_course.course_par, new_round.scores, nine_hole)]
-    result_new = calculate_whs_handicap(new_round.scores, current_course.slope_rating, course_rating, current_course.course_par, old_SDs, current_course, old_hdc_2021, nine_hole)
+    handicaps = [calculate_ega_handicap(old_hdc_2020, holes_played, slope_rating, course_rating, course_par, scores, nine_hole)]
+    result_new = calculate_whs_handicap(scores, slope_rating, course_rating, course_par, old_SDs, holes_played, old_hdc_2021, nine_hole)
     handicaps.append(result_new[0])
     handicaps.append(result_new[1])
     return handicaps
