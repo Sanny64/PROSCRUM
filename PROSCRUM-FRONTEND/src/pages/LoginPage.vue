5326441ea812d905cab6de64b557a2d18b860f28
Sammynator <sgerules@gmail.com>
2025-03-13 16:45:36 +0100

    added foundational design to Login page

--- PROSCRUM-FRONTEND/src/pages/LoginPage.vue
+++ PROSCRUM-FRONTEND/src/pages/LoginPage.vue
@@ -1,61 +1,55 @@
 <script setup lang="ts">
-import {inject, type Ref} from "vue";
+import { inject, type Ref } from 'vue'
 
-const activeUserAPI = inject<Ref<User | 'INVALID'>>("activeUser", ref("INVALID"));
-const refreshActiveUser = inject<() => Promise<void>>("refreshActiveUser");
+const activeUserAPI = inject<Ref<User | 'INVALID'>>('activeUser', ref('INVALID'))
+const refreshActiveUser = inject<() => Promise<void>>('refreshActiveUser')
 
-import {apiCallLogin} from '@/composables/api-call-login.ts'
+import { apiCallLogin } from '@/composables/api-call-login.ts'
 
-import type {LoginData, User} from '@/types/types.ts'
-import {nextTick, onMounted, ref, watch} from "vue";
-import {useRouter} from "vue-router";
+import type { LoginData, User } from '@/types/types.ts'
+import { nextTick, onMounted, ref, watch } from 'vue'
+import { useRouter } from 'vue-router'
 
-
-const router = useRouter();
+const router = useRouter()
 
 const loginData: LoginData = {
   username: 'jan@test.de',
-  password: '1234'
+  password: '1234',
 }
-
-
-
 
 async function login(loginData: LoginData) {
-  console.log("1: Login", loginData);
-  await apiCallLogin(loginData);
-  await refreshActiveUser?.();
-  console.log("5 activeUser", activeUserAPI.value);
-  await router.push("/course");
+  console.log('1: Login', loginData)
+  await apiCallLogin(loginData)
+  await refreshActiveUser?.()
+  console.log('5 activeUser', activeUserAPI.value)
+  await router.push('/course')
 }
-
 
 function logout() {
-  localStorage.setItem("activeToken", "")
+  localStorage.setItem('activeToken', '')
   activeUserAPI.value = 'INVALID'
 }
-
-
-
 </script>
 
 <template>
-  <input type="text" v-model="loginData.username" placeholder="Email">
-  <input type="password" v-model="loginData.password" placeholder="Password">
-  <button @click="login(loginData)">Login</button>
+  <div class="main">
+    <div class="wrapper">
+      <div v-if="activeUserAPI != 'INVALID'">
+        <h1>User: {{ activeUserAPI?.first_name }} {{ activeUserAPI?.last_name }}</h1>
+      </div>
+      <div v-else>
+        <h1 id="title">Anmeldung</h1>
+      </div>
 
-<div v-if="activeUserAPI != 'INVALID'">
-  <h1>User: {{activeUserAPI?.first_name}} {{activeUserAPI?.last_name}}</h1>
-</div>
-<div v-else>
-  <h1>User: Not logged in</h1>
-</div>
-
-  <button @click="logout()">Logout</button>
-
+      <div class="inputs">
+        <input type="text" v-model="loginData.username" placeholder="Email" />
+        <input type="password" v-model="loginData.password" placeholder="Password" />
+      </div>
+      <button id="loginButton" @click="login(loginData)">Login</button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-@import "../style/LoginPage.css";
-
+@import '../style/LoginPage.css';
 </style>
--- PROSCRUM-FRONTEND/src/pages/SignupPage.vue
+++ PROSCRUM-FRONTEND/src/pages/SignupPage.vue
@@ -1,38 +1,35 @@
 <script setup lang="ts">
-
-import RoundsChart from "@/components/rounds-chart.vue";
-import type {LoginData, UserCreate} from "@/types/types.ts";
-import {apiCallLogin, } from "@/composables/api-call-login.ts";
-import {apiCallUser} from "@/composables/api-call-user.ts";
+import RoundsChart from '@/components/rounds-chart.vue'
+import type { LoginData, UserCreate } from '@/types/types.ts'
+import { apiCallLogin } from '@/composables/api-call-login.ts'
+import { apiCallUser } from '@/composables/api-call-user.ts'
 const { addUserAPI } = apiCallUser()
 const userCreate: UserCreate = {
-  email: "jakobfischer@gmail.com",
-  password: "1234",
+  email: 'jakobfischer@gmail.com',
+  password: '1234',
   role_id: 4,
-  first_name: "Jakob",
-  last_name: "Fischer"
+  first_name: 'Jakob',
+  last_name: 'Fischer',
 }
 
 async function signup(userCreate: UserCreate) {
-  await addUserAPI(userCreate); // API-Call abwarten
+  await addUserAPI(userCreate) // API-Call abwarten
 }
-
 </script>
 
 <template>
+  <div class="main">
+    <p id="title">Registrierung</p>
+    <input type="text" v-model="userCreate.first_name" placeholder="Vorname" />
+    <input type="text" v-model="userCreate.last_name" placeholder="Nachname" />
+    <input type="number" v-model="userCreate.role_id" placeholder="Role" />
+    <input type="text" v-model="userCreate.email" placeholder="Email" />
+    <input type="password" v-model="userCreate.password" placeholder="Password" />
 
-  <input type="text" v-model="userCreate.first_name" placeholder="Vorname">
-  <input type="text" v-model="userCreate.last_name" placeholder="Nachname">
-  <input type="number" v-model="userCreate.role_id" placeholder="Role">
-  <input type="text" v-model="userCreate.email" placeholder="Email">
-  <input type="password" v-model="userCreate.password" placeholder="Password">
-
-  <button @click="signup(userCreate)">Registriren</button>
-
+    <button @click="signup(userCreate)">Registrieren</button>
+  </div>
 </template>
 
 <style scoped>
-@import "../style/SignupPage.css";
-
-
+@import '../style/SignupPage.css';
 </style>
